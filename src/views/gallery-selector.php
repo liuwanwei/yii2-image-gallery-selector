2b73a8d4d6929191f6338edbc5e69d0617ab90c0
liuwanwei <liuwanwei@gmail.com>
2016-05-20 15:41:19 +0800

    增加搜索框，支持根据图片名字进行搜索

--- src/views/gallery-selector.php
+++ src/views/gallery-selector.php
@@ -4,7 +4,7 @@
 
 <div id="<?=$id?>" class="gallery-selector-widget">
     <div class="clearfix">
-        <div class="form-group">
+        <div>
             <!-- <label for=""><?=$title?></label> -->
             <div class="no-image-selected" style="display:none">没有选择图片</div>
             <div class="selected-images">
@@ -21,43 +21,120 @@
     <div id="<?=$id?>-modal" class="modal fade">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
-                <div class="modal-header">
-                    <button class="close" data-dismiss="modal">&times;</button>
+                <div class="modal-header">                    
+
+                    <a class="close" data-dismiss="modal">&times;</a>
                     <h4><?=$modalTitle?></h4>
+
+                    <form class="form-inline">
+                      <div class="form-group">
+                        <!-- <label for="exampleInputName2">名字</label> -->
+                        <input type="text" class="form-control" id="image-name" placeholder="输入图片名字进行搜索">
+                      </div>
+                      <!-- <button type="submit" class="btn btn-default">查询</button> -->
+                    </form>
+
                 </div>
 
                 <div class="modal-body">
-                   <!--  <ul class="nav nav-tabs" data-tabs="tabs" style="margin-bottom: 10px;">
-                        <li class="active"><a href="#upload_new" data-toggle="tab">Upload</a></li>
-                        <li><a href="#select_from_gallery" data-toggle="tab">Select from Gallery</a></li>
-                    </ul> -->
-                    <!-- <div class="tab-content"> -->
-                        <!-- <div class="tab-pane active gallery-upload-container" id="upload_new">
-                            <div class="images-list">
-                                <div class="image-placeholder" data-upload-key="<?=$uploadKey?>">
-                                    <span class="glyphicon glyphicon-plus"></span>
+                    <div class="tab-pane gallery-selector-container" id="select_from_gallery">
+                        <div class="images-list">
+                            <?php foreach ($images as $index => $image): ?>
+                                <div class="gallery-selector-image" data-image-id="<?=$image['id']?>" data-image-name="<?=isset($image['name']) ? $image['name'] : $id . '-image-' . $index ?>" style="background-image: url('<?=$image['url']?>');" data-image-url="<?=$image['url']?>">
+                                    <span class="glyphicon glyphicon-check"></span>
+                                    <h5 style="position:relative; left:10px;"><?= $image['name'] ?></h5>
                                 </div>
-                            </div>
-                            <button type="button" class="btn btn-primary gallery-image-select" style="margin-left: 5px; margin-top: 10px;">Select</button>
-                        </div> -->
-                        <div class="tab-pane gallery-selector-container" id="select_from_gallery">
-                            <div class="images-list">
-                                <?php foreach ($images as $index => $image): ?>
-                                    <div class="gallery-selector-image" data-image-id="<?=$image['id']?>" data-image-name="<?=isset($image['name']) ? $image['name'] : $id . '-image-' . $index ?>" style="background-image: url('<?=$image['url']?>');" data-image-url="<?=$image['url']?>">
-                                        <span class="glyphicon glyphicon-check"></span>
-                                    </div>
-                                <?php endforeach ?>
-                            </div>
-                            <button type="button" class="btn btn-primary gallery-image-select" style="margin-left: 5px; margin-top: 10px;">确定</button>
+                            <?php endforeach ?>
                         </div>
-                    <!-- </div> -->
+                        <button type="button" class="btn btn-primary gallery-image-select" style="margin-left: 5px; margin-top: 10px;">确定</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
+<script type="text/javascript">
 <?php
+    // 生成所有待选图片的 js 数据对象，用于筛选。
+    // 由于界面上摆放了 5 个类似控件，所以要用不同的名字区分各自对应的可选图片数组.
+    $imageObjects = 'images_' . str_replace('-', '_', $relatedId);
+    echo "var $imageObjects = [";
+    foreach ($images as $image) {
+        echo "\n  {\n";
+        echo "    id:" . $image['id'] . ",";
+        echo "    name:'" . $image['name'] . "',";
+        echo "    url:'" . $image['url'] . "'";
+        echo "\n  },";
+    }
+    echo "];";
+?>
+  
+/**
+ *
+ * 根据用户输入的内容，筛选图片，并展示在界面中中
+ *
+ */
+ function filterImages(images, name, divId){
+    console.log('log in filterImages:' + name);
+
+    console.log(images);
+
+    filteredImages = [];
+    images.forEach(function (item, index, array){
+        console.log(item.name);
+        if (item.name.includes(name)) {
+            filteredImages.push(item);
+        }
+    });
+
+    // 清空原先的图片列表
+    let elements = $('div #' + divId + ' .gallery-selector-image');
+    elements.remove();
+
+    // 把筛选后的图片一张张加入列表
+    let imagesList = $('div #' + divId + ' .images-list');
+    console.log(imagesList);
+    filteredImages.forEach(function(item, index, array){
+        imageDiv = '<div class="gallery-selector-image" ' + 
+            'data-image-id="' + item.id + '" ' + 
+            'data-image-name="' + item.name + '" ' + 
+            'style="background-image: url(' + '\'' + item.url + '\');" ' + 
+            'data-image-url="' + item.url + '"> ' + 
+            '<span class="glyphicon glyphicon-check"></span>' + 
+            '<h5 style="position:relative; left:10px;">' + item.name + '</h5></div>';
+        console.log(imageDiv);
+        imagesList.add(imageDiv).appendTo(imagesList);
+    });
+ }
+    
+</script>
+
+
+<?php
+
+// 这段代码用到了 jQuery，所以必须通过 View::registerJS 注入
+$script = <<<JS
+    $(document).ready(function(){
+        $('#$id input#image-name').keydown(function(event){
+            // 用户按下回车后，开始根据输入的内容筛选照片
+            if (event.keyCode == 13) {
+                filterImages($imageObjects, $(this).val(), '$id');
+                event.preventDefault();
+                return false;
+            }
+        });
+    });
+
+    $('div #' + '$id').on('hidden.bs.modal', function(){
+        // 关闭界面前，恢复最开始的图片列表，并清除搜索内容
+        filterImages($imageObjects, '', '$id');
+        $('#$id input#image-name').val('');
+    });
+JS;
+$this->registerJS($script, \yii\web\View::POS_END);
+
+
 $script = <<<JS
     var _context = $("#{$id}");
     var _container = _context.find('.gallery-selector-container .images-list');
